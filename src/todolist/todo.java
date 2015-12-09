ca3701a2e4f1706887bf9da76476efd5c846e03a
Branden Soropia <b.soropia@gmail.com>
2015-12-09 15:34:27 -0500

    Implemented all setters and getters. Made a tiny test for it as well.

--- src/todolist/todo.java
+++ src/todolist/todo.java
@@ -23,9 +23,59 @@ public class todo {
 		this.task = task;
 	}
 	
+	private int getStatus() {
+		return this.status;
+	}
+	
+	private void setStatus(int status) {
+		this.status = status;
+	}
+	private int getTaskNum() {
+		return this.taskNum;
+	}
+
+	private void setTaskNum(int taskNum) {
+		this.taskNum = taskNum;
+	}
+
+	private todo getNextTodo() {
+		return this.nextTodo;
+	}
+
+	private void setNextTodo(todo nextTodo) {
+		this.nextTodo = nextTodo;
+	}
+
+	private todo getPrevTodo() {
+		return this.prevTodo;
+	}
+
+	private void setPrevTodo(todo prevTodo) {
+		this.prevTodo = prevTodo;
+	}
+	
+	private void changeStatus() {
+		if (this.status == 0) {
+			this.status = 1;
+			System.out.println("Changed status from 0 to 1");
+		} else {
+			this.status = 0;
+			System.out.println("Changed status from 1 to 0");
+		} 
+	}
+	
 	@Override
 	public String toString() {
 		return "Task: " + this.task + "\nStatus: " + this.status + "\nTask Number: " + this.taskNum + "\n";
 	}
-	
+// small quick test 
+//	public static void main(String[] args) {
+//		todo todo1 = new todo("Test todo class.", 0, null, null);
+//		todo todo2 = new todo("Next is set.", 0, null, todo1);
+//		todo2.setTaskNum(1);
+//		todo1.setNextTodo(todo2);
+//		System.out.println(todo1.toString());
+//		System.out.println(todo1.getNextTodo().toString());
+//		System.out.println("Prev node is " + todo1.getNextTodo().getPrevTodo().toString());
+//	}
 }
